5edc8270995c7b118a40361b88e8e633c2d7105f
Iori Matsuhara <matsuhar@gmail.com>
2016-01-23 12:23:19 +0100

    ++

---
+++ keyboard/ergodox_ez/constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+enum {
+  BASE,
+  EDIT,
+  SYMB,
+  MDIA,
+  NUMB,
+};
+
+enum {
+  SWITCH_APP,
+  SWITCH_TAB,
+  SWITCH_BUFFER,
+  TOGGLE_SYMB_LAYER,
+  DELETE_BEG_LINE,
+  DELETE_END_LINE,
+};
+
+#endif
