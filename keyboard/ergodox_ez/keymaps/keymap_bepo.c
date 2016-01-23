1989c5eccc5c08d65aa6de4c2365483cf06e73ce
Iori Matsuhara <matsuhar@gmail.com>
2016-01-23 14:24:26 +0100

    ++

--- keyboard/ergodox_ez/keymaps/keymap_bepo.c
+++ keyboard/ergodox_ez/keymaps/keymap_bepo.c
@@ -34,16 +34,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_QUOT, KC_QUOT, LALT(KC_LSFT), KC_LEFT,KC_RGHT,
                   ALT_T(KC_APP), KC_LGUI,
                   KC_HOME,
-                  KC_SPC,KC_BSPC,KC_END,
+                  KC_ENT, KC_BSPC, KC_END,
                   // right hand
                   KC_RGHT, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-                  KC_Y, KC_Y, KC_V, KC_D, MO(SYMB), KC_J, KC_BSLS,
+                  KC_Y, KC_Y, KC_V, KC_D, KC_L, MO(SYMB), KC_BSLS,
                   KC_C, KC_T, KC_S, KC_R, KC_N, MO(EDIT),
                   MEH_T(KC_NO), KC_N, KC_M, KC_COMM, KC_RSFT, KC_RSFT, CTL_T(KC_SLSH),
                   KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_FN1,
                   KC_LALT, CTL_T(KC_ESC),
                   KC_PGUP,
-                  KC_PGDN,KC_TAB, KC_ENT
+                  KC_PGDN, KC_TAB, KC_SPC
                   ),
 
   /*
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_TRNS, KC_TRNS, KC_TRNS,
                   // right hand
                   KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-                  KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_TRNS, KC_TRNS,
-                  KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS,
-                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                  KC_TRNS, KC_TRNS, LCTL(KC_V), KC_HOME, KC_END, KC_TRNS, KC_TRNS,
+                  LCTL(KC_C), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS,
+                  KC_TRNS, KC_TRNS, LCTL(KC_X), LCTL(KC_LEFT), LCTL(KC_RIGHT), KC_TRNS, KC_TRNS,
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                   KC_TRNS, KC_TRNS,
                   KC_TRNS,
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYMB] = KEYMAP(
                   // left hand
                   KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-                  KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+                  KC_TRNS,KC_TRNS,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
                   KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
                   KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
                   KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -119,8 +119,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_TRNS,KC_TRNS,KC_TRNS,
                   // right hand
                   KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-                  KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                  KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+                  KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_TRNS, KC_TRNS, KC_F12,
+                  KC_DOWN, KC_4,   KC_5,    KC_6,    KC_TRNS, KC_TRNS,
                   KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                   KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
                   KC_TRNS, KC_TRNS,
@@ -152,7 +152,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
   case DELETE_END_LINE:
     if (record->event.pressed) {
-      return MACRO(D(LSFT), T(END), U(LSFT), T(BSPC), END);
+      return MACRO(D(LSFT), T(END), U(LSFT), T(DEL), END);
     }
     break;
 
